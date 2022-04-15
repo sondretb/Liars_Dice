47a06e370eb3b4ca15196780118562f0c967b64c
Clas Olaf Steinbru Andersen <clas@claso.io>
2022-04-15 17:02:58 +0200

    feat: added typescript and socket.io to server

---
+++ server/dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+const { Server } = require("socket.io");
+const io = new Server();
+io.on("connection", () => {
+    console.log("Someone connected!");
+});
+io.listen(3000);
+console.log("Listening on 3000");
+//# sourceMappingURL=server.js.map
